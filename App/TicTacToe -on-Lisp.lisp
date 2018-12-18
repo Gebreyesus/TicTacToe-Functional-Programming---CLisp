af63973718d1f39b5abbd515d5734053ccc5339a
Gebreyesus <beteab@live.com>
2018-12-18 13:08:02 -0600

    found unpushed changes during revision

--- App/TicTacToe -on-Lisp.lisp
+++ App/TicTacToe -on-Lisp.lisp
@@ -7,55 +7,139 @@
 (defparameter *gameboard-sample* (list 'x 'x 'x '- '0 '0 '- '- '0))
 
 ;;-------------------------------------------------------------------------
-;;; set the maove merker for the first move
-(setf *marker* :X)
-(setf *player* "Player")
-
-;;-------------------------------------------------------------------------
-;; Create the playing board and setting intial values to mark each square
-(defun create-board ()
+;; Create the Playing board as a array with 9 spots(array indexes).
+;; We will label each spot to allow users to make Play by entering spot number
+(defun InitialiseBoard () 
 	(setf *board* (make-array 9 :initial-contents '(1 2 3 4 5 6 7 8 9))))
 
 ;;-------------------------------------------------------------------------
-;; prints the borad in proper format
-(defun print-board (board)
-	(do ((i 0(+ i 1)))
-		((+ i 9) 'done)
-		(format t " ~A" (nth i board))
-	)
-)
+;;; Defines funtion to selct squares from entry
+(defun LookUpBoard (cell) (aref *board* (1- cell)))
+
+;;-------------------------------------------------------------------------
+;; Setting values :- Player 1 will be represented by symbol ' X ' 
+(setf *P1-marker* :X) 
+(setf *Player* "First Player")
+
+;;-------------------------------------------------------------------------
+;; Next players turn --> change players
+(defun NextPlayer ()
+	(if (equal *P1-marker* :X) (setf *P1-marker* :O) (setf *P1-marker* :X))
+	(if (equal *Player* "Player 1")	
+		(setf *Player* "Player 2") (setf *Player* "Player 1")))
+
+;;-------------------------------------------------------------------------
+;; handling invlaid entry
+(defun invalidEntry ()
+	(format t " ~% Square is occupied - Enter Again! ~%")
+	(draw-board) (force-output nil)	(checkUserSelection (readSelection)))
+
+;;-------------------------------------------------------------------------
+;; Marks selcted spot if selection is valid
+(defun Mark (selection)
+	(if (numberp (LookUpBoard Entry)) 
+	(setf (LookUpBoard Entry) *P1-marker*) (invalidEntry) ) )
+
+;;-------------------------------------------------------------------------
+;;; Show user the board and read selection into 'Player 1'
+(defun readSelection ()
+	(draw-board)
+	(format t "~% Enter a square ID(Number) to mark ~%" *Player*) 
+	(force-output nil)
+	(parse-integer (read-line *query-io*) :junk-allowed t))
+
+;;-------------------------------------------------------------------------
+;; Determines if three items are all equal to each other		
+(defun LookUpBoard (cell)  (aref *board* (1- cell)))
 
 ;;-------------------------------------------------------------------------
 ;; Determines if three items are all equal to each other
+;; row or column or diagonal values(a,b,c) are passed as to be compared
 (defun threequal (a b c)
   (and (equal a b) (equal b c)))
 
 ;;-------------------------------------------------------------------------
-;; Determines if a list representing a row or column or diagonal
-;; of a tic-tac-toe board is a victory
-;; FIXME: needs to return false for three '- items in the list.
-(defun victory (alist) 
-  (and (equal (first alist) (second alist))
-       (equal (second alist) (third alist))))
+;; re-run the game to Play again
+(defun PlayAgain ()
+	(create-board)	(format t "New Game - Start ~a.~%~%" *Player*)
+	(Play))
 
 ;;-------------------------------------------------------------------------
-;; Returns a list consisting of the nth row (zero-based) of
-;; a tic-tac-toe board
-(defun grab-row (board row)
-  (let ((x (* 3 row)))
-    (list (nth x board)
-          (nth (+ x 1) board)
-          (nth (+ x 2) board))
-  )
-)
+;; Check for a win by brute-force checking all permutations on a line
+(defun Winnner ()
+	(or (threequal 1 2 3)
+		(threequal 1 4 7)
+		(threequal 1 5 9)
+		(threequal 2 5 8)
+		(threequal 3 6 9)
+		(threequal 3 5 7)
+		(threequal 4 5 6)
+		(threequal 7 8 9)))
 
 ;;-------------------------------------------------------------------------
-;; Returns a list consisting of the nth column (zero-based) of
-;; a tic-tac-toe board
-(defun grab-col (board col)
-  (list (nth col board)
-        (nth (+ col 3) board)
-        (nth (+ col 6) board)))
+;; Check of it is a draw 
+(defun Draw ()
+	(when (and
+			(not (numberp (aref *board* 0)))
+			(not (numberp (aref *board* 1)))
+			(not (numberp (aref *board* 2)))
+			(not (numberp (aref *board* 3)))
+			(not (numberp (aref *board* 4)))
+			(not (numberp (aref *board* 5)))
+			(not (numberp (aref *board* 6)))
+			(not (numberp (aref *board* 7)))
+			(not (numberp (aref *board* 8))) ) t))
+	
+;;-------------------------------------------------------------------------
+;; defines user selection. only accepts selction 0-9, 
+;; will ask user again for wrong entries
+(defun checkUserSelection (selection)
+	(if (and (numberp selection)  (> selection 0) (< selection 10))
+		(Mark selection)
+		(progn (format t "~% Entry is Invalid ~%")
+		(check-selection (read-selection)))))
 
 ;;-------------------------------------------------------------------------
+;;; Draw the board
+;;; If numbersp is t, draw the numbers on the board
+(defun draw-board ()
+	(format t "=====================~%")
+	(format t "||   ~a |   ~a |   ~a ~%||" 
+			(LookUpBoard 1) (LookUpBoard 2) (LookUpBoard 3))
+	(format t "||     |      |     ~%||")
+	(format t "||___________________~%||")
+	(format t "||   ~a |   ~a |   ~a ~%||" 
+			(LookUpBoard 4) (LookUpBoard 5) (LookUpBoard 6))
+	(format t "||     |      |     ~%||")
+	(format t "||___________________~%||")
+	(format t "||   ~a |   ~a |   ~a ~%||" 
+			(LookUpBoard 7) (LookUpBoard 8) (LookUpBoard 9))
+	(format t "=====================~%||")
+
+;;-------------------------------------------------------------------------
+;; If game is not paused(continue=true(t)) we keep asking for next move
+;; If current move doesn't create stalemate or a win -> recursively play
+;; If the current move creates draw or a win -> prmpts for new gaem
+(defun Play (&optional ContinueGame)
+  (when ContinueGame (NextPlayer))
+  (checkUserSelection (readSelection))
+  (when (and (not (Winnner)) (not (Draw))) (Play t))
+  (when (Winnner) (progn (format t "~a is the Winner! " *Player*)
+      (force-output nil) (if (y-or-n-p "Restart Game!? -> Enter Y or N") 
+		(PlayAgain)
+	  	(quit))) )
+  (when (Draw) 
+  	(if (y-or-n-p "~%~% Its a Draw! ~% Enter Y to Restart Game!")
+	(PlayAgain)
+	(quit))) )
+
+
+;;-------------------------------------------------------------------------
+;;; Start the game and setup the board and return function Play
+(defun StartGame () 
+	(format t "This is Tic Tac Toe") (InitialiseBoard) (Play nil))
+
+;;-------------------------------------------------------------------------
+;;Start Game called here
+(StartGame)
 
--- README.md
+++ README.md
@@ -1,29 +1,26 @@
-# README #
+## README #
+* Beteab Gebru
+* CSC 345 - Compilers
+* Functional Programming Assignment using Common Lisp
 
-TicTacToe Game Implementation using Lisp 
+## Description 
+This is a simple program of a game Tic Tac Toe. 
+It was developed using the functions programming language paradign in CommonLisp. 
+
+## In its current state 
+* 2-player game .
+* game will check for a win/illegalmove/draw after every move by player 1 or player 2
+* will declare a winner and pronpt user if wanting to play again
+
+## --> future possibilities
+* At completion it will be able to allows users to play against the computer or Opposition
+* The computer will avoid making a losing move unless no options exist. 
+* The computer will make a move by checking if next open position is a losing move. 
+* if next opening is not a losing move it will decide to take it as the next best move.
 
 ### What is this repository for? ###
-#### Functional Programming
-* Quick summary
-* Version
-* [Learn Markdown](https://bitbucket.org/tutorials/markdowndemo)
+TicTacToe Game Implementation using Common-Lisp 
 
-### How do I get set up? ###
-
-* Summary of set up
-* Configuration
-* Dependencies
-* Database configuration
-* How to run tests
-* Deployment instructions
-
-### Contribution guidelines ###
-
-* Writing tests
-* Code review
-* Other guidelines
 
 ### Who do I talk to? ###
-
-* Repo owner or admin Beteab Gebru
-* Other community or team contact
+* Repo owner/admin : Beteab Gebru
